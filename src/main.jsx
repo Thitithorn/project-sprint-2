40354328f84cf4d8e0dc513808886a339ac778df
Thitithorn <Thitithorn.su@gmail.com>
2024-11-07 11:02:13 +0700

    create-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Signin from "./Signin";
+import Signup from "./Signup";
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/Navbar",
+    element: <Navbar />, 
+  },
+  {
+    path: "/Footer",
+    element: <Footer />, 
+  },
+  {
+    path: "/Signin",
+    element: <Signin />, 
+  },
+  {
+    path: "/Signup",
+    element: <Signup />, 
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
